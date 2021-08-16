d80f944619cabbd8cdd2c5a37479c5b8c01c36f4
Robert <bobgravity1@gmail.com>
2021-08-16 20:18:28 +0100

    text box shadow in progress bar

--- src/components/progressBar/progress_bar.js
+++ src/components/progressBar/progress_bar.js
@@ -5,25 +5,35 @@ const ProgressBar = (props) => {
     const containerStyles = {
       height: 16,
       width: '100%',
-      backgroundColor: "black",   
+      backgroundColor: "white",   
       margin:0,
     } 
     const fillerStyles = {
       height: '100%',
+      boxShadow:'rgba(0, 0, 0, 0.45) 10px -30px 8px -28px inset',
       width: `${completed}%`,
       transition:'all .3s ease-out',
       backgroundColor: bgcolor,
       borderRadius: 'inherit',
       textAlign: 'right', 
+      borderTopRightRadius:3,
+      borderBottomRightRadius:3,
     }
     const labelStyles = {
-        border:"none",
-      padding: 5,
+      border:"none",
+      padding: 3,
       color: 'white',
       fontWeight: 'bold'
     }
+    const progress = {
+      fontFamily:'Roboto',
+      color: 'black',
+      textAlign:'center',
+      fontWeight: 'light'
+    }
     return (
     <> 
+
       <div style={containerStyles}>
         <div style={fillerStyles}>
           <span className='x font' style={labelStyles}>{`${completed}%`}</span>
