cbb6b5e3aa3fa6d5b9e6b20d04b862390157fb9f
Robert <bobgravity1@gmail.com>
2021-08-18 21:21:03 +0100

    end test design

--- src/pages/endtest.js
+++ src/pages/endtest.js
@@ -4,19 +4,23 @@ import logo from '../images/logo.svg'
 // components
 import Scoreoptionbuttons from '../components/button/scoreoptionbuttons'
 // svg - pink brain
-import pinkBrain from '../images/pinkbrain.svg'
+import pinkBrain from '../images/header-meta-icon.svg'
 
 const endtest = () => {
+    const pinkBrainStyle={
+        height:'5rem',
+    }
     return (
         <>
     <Layout>
         <main className='endtest'>
             <article className='endtest-container'>
-            <h1 className='endtest-title'>Congratulations! You Finished the Test!</h1>
-            <p className='endtest-subtitle'>To view you results press one of the options below:</p>
-        <Scoreoptionbuttons />
+                <img style={pinkBrainStyle} src={pinkBrain} />
+                <h1 className='endtest-title'>Congratulations! You Finished the Test!</h1>
+                <p className='endtest-subtitle'>To view you results press one of the options below:</p>
+      
+            <Scoreoptionbuttons />
             </article>
-  
         </main>
     </Layout>
     </>
