8123caae623e9df071669237a8a1dd09bb76be91
andrew <andrew@Macs-iMac.local>
2021-09-09 10:17:05 -0400

    Fixed up breakdown section

--- src/text/descriptionPage.js
+++ src/text/descriptionPage.js
@@ -4,18 +4,18 @@ export const bannerText={
     extraTitle:'Get More',
     bannerTitle:'From Your Score',
     bannerSubtitle:'We analyze your IQ score with data-driven graphs and personalized evaluations.',
-    bannerSubtitleTwo:'Only 2.99$',
+    bannerSubtitleTwo:'Only 4̶.̶9̶9̶ $2.99',
     buttonText:'Cognitive Analysis'
 }
 
 export const blackBarText={
-    title:'Best Free IQ Test',
-    subtitle:`"Best Test Out There"`,
+    title:'Cognitive Analysis',
+    subtitle:`"Our optional section which aims to provide you with a more complete understanding of your IQ."`,
 }
 
 export const blackBarTextButton={
     title:'Purchase Cognitive Analysis',
-    subtitle:`Only 2.99$!`,
+    subtitle:`Only $2.99 !`,
     stripeSubtitle:`Click Below`,
     checkoutSubtitle:`Checkout Securely Powered by Stripe`,
     buttonText:'Score Analysis',
@@ -51,6 +51,6 @@ export const threeColumnText=[
     ]
 
     export const paragraphText=[
-        `first Many tests seek to measure what you already know. This is referred to as Crystallized IQ, or Crystallized Intelligence. Crystallized IQ can be expanded upon overtime, whereas Fluid IQ, or fluid intelligence/fluid reasoning classification attempts to measure what you have the capability to learn. Prior to the invention of the first IQ test, there was no viable way to measure intelligence (IQ), leading people to believe in many false presumptions about IQ and IQ tests; such as head size seen as being related to intelligence, intelligence being routinely correlated with birth order, and left handed people being more intelligent than their right handed counterparts. To read more about previous misconceptions about intelligence born from these previous IQ tests, click here. rder, and left handed people being more intelligent than their right handed counterparts. To read more about previous misconceptions about intelligence born from these previous IQ tests, click here. rder, and left handed people being more intelligent than their right handed counterparts. To read more about previous misconceptions about intelligence born from these previous IQ tests, click here. rder, and left handed people being more intelligent than their right handed counterparts. To read more about previous misconceptions about intelligence born from these previous IQ tests, click here. rder, and left handed people being more intelligent than their right handed counterparts. To read more about previous misconceptions about intelligence born from these previous IQ tests, click here. misconceptions about intelligence born from these previous IQ tests, click here. rder, and left handed people being more intelligent than their right handed counterparts. To read more about previous misconceptions about intelligence born from these previous IQ tests, click here.`,
+        `first Many tests seek to measure what you already know. This is referred to as Crystallized IQ, or Cryst        allized Intelligence. Crystallized IQ can be expanded upon overtime, whereas Fluid IQ, or fluid intelligence/fluid reasoning classification attempts to measure what you have the capability to learn. Prior to the invention of the first IQ test, there was no viable way to measure intelligence (IQ), leading people to believe in many false presumptions about IQ and IQ tests; such as head size seen as being related to intelligence, intelligence being routinely correlated with birth order, and left handed people being more intelligent than their right handed counterparts. To read more about previous misconceptions about intelligence born from these previous IQ tests, click here. rder, and left handed people being more intelligent than their right handed counterparts. To read more about previous misconceptions about intelligence born from these previous IQ tests, click here. rder, and left handed people being more intelligent than their right handed counterparts. To read more about previous misconceptions about intelligence born from these previous IQ tests, click here. rder, and left handed people being more intelligent than their right handed counterparts. To read more about previous misconceptions about intelligence born from these previous IQ tests, click here. rder, and left handed people being more intelligent than their right handed counterparts. To read more about previous misconceptions about intelligence born from these previous IQ tests, click here. misconceptions about intelligence born from these previous IQ tests, click here. rder, and left handed people being more intelligent than their right handed counterparts. To read more about previous misconceptions about intelligence born from these previous IQ tests, click here.`,
         `second IQ assessments have been performed for purposes of job recruitment, military service induction, and cases of identifying intellectual disability. IQ tests are performed to assess your intellectual ability, or your IQ; this is done by comparison of your IQ test score relative to a wider population of test score data. If you score an test score within the higher end on an IQ test, it is an indication of your intellectual abilities relative to that area. Of the many abilities measured by IQ tests, including linguistic ability and numerical reasoning, general logic and pattern recognition, the extent to which your final IQ test score is reflective of your real world abilities tends to fall the more variables you try to measure. To solve this issue, we only measure three variables in coming to our overall IQ test score. All purchases of our IQ test service help to fund more of our IQ test research, IQ test development and IQ test validity. For more information about purchasing our IQ test service, If you are scored as having an IQ test result within a certain unexpected range of intelligence, you should not take that as a reflection of what you can accomplish. The world is your oyster, and making IQ tests free and accessible to everybody is our specialty. To read more about our research, here.  you should not take that as a reflection of what you can accomplish. The world is your oyster, and making `,
       ]
