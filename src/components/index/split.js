7d838a82a017def458641f41e8f9f6ae623feba0
John Doe <andrewotoole@Andrews-MacBook-Pro.local>
2022-01-10 21:25:10 +0000

    fixed 1.99 to 2.99 and a few other UX additions

--- src/components/index/split.js
+++ src/components/index/split.js
@@ -6,20 +6,20 @@ const Split = ({imgOne, imgTwo}) => {
 <>   
     <div className="split">
         <div className="split-piece split-left">
-            <h1>Knowledge Is Power. <br />Why Not Test Your Abilities?</h1>
+            <h1>Cognitive Analysis <br />Meaningful Results, Backed by Data</h1>
                <div className="split-left-image">
                     <img src={imgOne} />
                     <p></p> 
                 </div>
-               <p>The average IQ is 100. Most people (about 68%) have an IQ between 85 and 115. What's your IQ?</p>
+               <p>The average IQ is 100. Roughly 68% of people have an IQ between 85 and 115. What's your IQ?</p>
         </div>
         <div className="split-piece split-right">
-            <h1>Get a Professional <br />Score Analysis for only 2.99$!</h1>
+            <h1>(Free result + Cognitive Analysis) <br />only $2.99! <br /> Get your complete IQ profile!</h1>
           
                 <div className="split-right-image">
                     <img src={imgTwo} />
                 </div>
-                <p>Purchase a full score breakdown for 2.99 at the end of the test! Every analysis is 100% unique. </p> 
+                <p>Purchase a full Cognitive Analysis for just $2.99 at the end of the test! Every analysis is 100% unique. </p> 
         </div>
     </div>
 </>
