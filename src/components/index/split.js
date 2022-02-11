47139c69879b11ca1b7b51fbada5bfd8421b0832
PP-blip <76789017+PP-blip@users.noreply.github.com>
2022-02-11 15:45:10 +0000

    reverted changes to see something

--- src/components/index/split.js
+++ src/components/index/split.js
@@ -8,7 +8,7 @@ const Split = ({imgOne, imgTwo}) => {
         <div className="split-piece split-left">
             <h1>IQ Score Analysis <br />Meaningful Results, <br /> Backed by Data</h1>
                <div className="split-left-image">
-                    <img src={imgOne} alt="data-chart"/>
+                    <img src={imgOne} />
                     <p></p> 
                 </div>
                <p>The average IQ is 100. Roughly 68% of people have an IQ between 85 and 115. What's your IQ?</p>
@@ -16,8 +16,8 @@ const Split = ({imgOne, imgTwo}) => {
         <div className="split-piece split-right">
             <h1>(IQ Score Analysis) <br />Complete IQ Profile for just <br /> $2.99! <br /> </h1>
           
-                <div className="split-right-img">
-                    <img src={imgTwo} alt="pricing-img" />
+                <div className="split-right-image">
+                    <img src={imgTwo} />
                 </div>
                 <p>Purchase a full Cognitive Analysis for just $2.99 at the end of the test! Every analysis is 100% unique. </p> 
         </div>
